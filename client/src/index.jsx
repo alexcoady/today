d4f9871eaddead2978c3c05b306b51e41037598e
Alex Coady <alexandercoady@me.com>
2016-09-28 22:13:02 +0100

    Added babel setup for react and created test component. This closes #18

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 console.log('hiya from index.jsx');
 
 (() => {
   console.log('and what?');
 })();
+
+const Test = ({ children }) => {
+
+  return (
+    <div className="whatever">
+      { children }
+    </div>
+  );
+};
+
+render(
+  <Test>
+    Just a silly test
+  </Test>,
+  document.getElementById('App')
+);
