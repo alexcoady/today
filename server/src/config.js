79210067ff972c9eee468c67695079638d4c6afc
Alex Coady <alexandercoady@me.com>
2016-10-06 23:02:33 +0100

    updates

---
+++ server/src/config.js
@@ -0,0 +1,5 @@
+export const SECRET = 'anyoldnonsensereally';
+export const MONGODB = 'mongodb://127.0.0.1/today-0-0-1';
+
+export const FACEBOOK_CLIENT_ID = '530905813771643';
+export const FACEBOOK_CLIENT_SECRET = '150b3126ee54e3c2108d6f9d8804291d';
