e9ff072ab0f80ff923849c19cc474eb9848cde6c
Alex Coady <alexandercoady@me.com>
2016-10-25 20:37:24 +0100

    updated days reducer

--- shared/days/reducer.js
+++ shared/days/reducer.js
@@ -1,21 +1,41 @@
 // NPM dependencies
+import _map from 'lodash/map';
+import _reduce from 'lodash/reduce';
+import _uniq from 'lodash/uniq';
 import { combineReducers } from 'redux';
 
 // Feature dependencies
 import * as t from './actionTypes';
 
-const all = (state = [], action) => {
+const all = (state = [], { type, payload }) => {
 
-  switch (action.type) {
+  switch (type) {
     case `${t.FETCH_ALL}_FULFILLED`:
-      return action.payload.data.data;
+      return _uniq(_map(payload.data.data, 'date'));
     case `${t.POST_DAY}_FULFILLED`:
-      return [...state, action.payload.data.data];
+      return _uniq([...state, payload.data.data.date]);
+  }
+
+  return state;
+};
+
+const byDate = (state = {}, { type, payload }) => {
+
+  switch (type) {
+    case `${t.FETCH_ALL}_FULFILLED`:
+
+      return _reduce(payload.data.data, (result, value) => {
+        return {...result, [value.date]: value};
+      }, {});
+
+    case `${t.POST_DAY}_FULFILLED`:
+      return {...state, [payload.data.data.date]: payload.data.data};
   }
 
   return state;
 };
 
 export default combineReducers({
-  all
+  all,
+  byDate
 });
--- shared/days/selectors.js
+++ shared/days/selectors.js
@@ -1,9 +1,14 @@
 // NPM
-// import { createSelector } from 'reselect';
+import _map from 'lodash/map';
+import { createSelector } from 'reselect';
 
 // Feature
 import * as constants from './constants';
 
-export const getAll = state => {
-  return state[constants.NAME].all;
-};
+export const getDatesArray = state => state[constants.NAME].all;
+export const getByDateObject = state => state[constants.NAME].byDate;
+
+export const getAll = createSelector(
+  [getDatesArray, getByDateObject],
+  (all, obj) => _map(all, date => obj[date])
+);
